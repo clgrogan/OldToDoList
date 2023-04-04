127d3d8d98b093bb270e1a9890b2116f3d19e2c6
Curt Grogan <curt.grogan@fdmgroup.com>
2023-04-04 09:38:36 -0400

    initial commit

---
+++ src/test/java/com/sbmaster/todo/GetItDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmaster.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetItDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
